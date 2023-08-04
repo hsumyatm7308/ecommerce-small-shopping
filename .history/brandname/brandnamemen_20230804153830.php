43d3153df8778a930ff77ac3e2c65fe90d9863af
Hsu Myat Moe <hsumyatm7308@gmail.com>
2023-08-04 17:07:51 +0630

    reponsive frg <4 Aug 2023>

---
+++ .history/brandname/brandnamemen_20230804153830.php
@@ -0,0 +1,47 @@
+<?php
+require_once "database.php";
+
+
+
+
+try {
+    global $conn;
+
+    $menstmt = $conn->prepare("SELECT fsletter FROM perfume WHERE category_name IN ('Men','Unisex') ORDER BY fsletter ASC");
+    $menstmt->execute();
+
+
+    $shownLetters = array();
+
+} catch (Exception $e) {
+    echo "Error Found: " . $e->getMessage();
+}
+
+
+
+
+
+?>
+
+
+
+<?php while ($row = $menstmt->fetch()): ?>
+    <?php
+    if (!in_array($row['fsletter'], $shownLetters)) {
+        $shownLetters[] = $row['fsletter'];
+        ?>
+        <?php
+
+        $letter = $row['fsletter'];
+        if (isset($_GET['letters']) && $_GET['letters'] === $letter) {
+            $backgroundClass = "bg-gray-400 text-white";
+        } else {
+            $backgroundClass = "bg-gray-100";
+        }
+        ?>
+       
+        <li class="w-7 h-7 flex justify-center items-center m-1 letter-link <?php echo $backgroundClass; ?>">
+            <a href="menfrg.php?menpage=1&letters=<?php echo urlencode($letter); ?>"><?php echo $letter; ?></a>
+        </li>
+    <?php } ?>
+<?php endwhile; ?>
