c1dd72e1dc8fd795ffaa49e7e04d73ed6a8f1d53
Hsu Myat Moe <hsumyatm7308@gmail.com>
2023-08-26 19:38:31 +0630

    information <26 Aug 2023>

---
+++ .history/removeitem_20230826193454.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once "database.php";
+require_once "temporaryid.php";
+$temp_customer_id = $_SESSION['id'];
+
+if (isset($_POST['action']) && $_POST['action'] === 'del') {
+    $id = $_POST['id'];
+    echo $id;
+
+
+    // $stmt = $conn->prepare('DELETE FROM addtocart WHERE perfume_id = :id');
+    // $stmt->bindParam(':id', $id);
+    // $stmt->execute();
+
+    // header("Location: shopcartpage.php");
+    // exit;
+
+    // if (isset($_POST['remove'])) {
+       
+        $stmt = $conn->prepare('DELETE FROM addtocart WHERE perfume_id = :id AND temporaryid = :temp');
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':temp', $temp_customer_id);
+        $stmt->execute();
+
+
+
+        // header("Location: shopcartpage.php");
+        // exit;
+    // }
+
+}
+?>
