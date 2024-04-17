46c303afc11d89c6bb4a6e8aa69a231a741792dd
Hsu Myat Moe <hsumyatm7308@gmail.com>
2024-04-18 00:43:58 +0630

    letter sidebar dynamic

--- app/models/Side.php
+++ app/models/Side.php
@@ -5,17 +5,35 @@ class Side
 
     private $db;
 
+    public $curmethodmodal;
     public function __construct()
     {
         $this->db = new Database();
-        // echo "connect";
+        $this->curmethodmodal = new Curitemid();
 
     }
 
     public function sidebaritems()
     {
 
-        $this->db->dbquery('SELECT * FROM items');
+
+
+        $curarray = function () {
+            $curmethod = $this->curmethodmodal->getmethod();
+
+            $curmethod = explode('?', $curmethod)[0];
+
+            if ($curmethod === 'allfragrance') {
+                $array = '(1,2,3)';
+            } elseif ($curmethod === 'lotions') {
+                $array = '(5,6)';
+            } else {
+                $array = '(7,8,9)';
+            }
+            return $array;
+        };
+
+        $this->db->dbquery('SELECT * FROM items WHERE category_id IN' . $curarray());
 
         return $this->db->getmultidata();
     }
